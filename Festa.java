3b9bdbd8732bfd075057b4ef8a91616a226222c6
Gorka <gorka32x@gmail.com>
2019-02-07 15:56:01 +0100

    Creacio nou constructor i getter rockandroll

--- Festa.java
+++ Festa.java
@@ -1,6 +1,15 @@
 public class Festa {
     
     Festa(int sexe, int drogues, int rockandroll){
+    
     }
     
+    Festa(int rockandroll){
+    
+    
+    }
+    
+    public int GetRockAndRoll(){
+    
+    }
 }
