0fd1854b93651752b39ba1f3ee648da5a364e3dd
Bernat <bernat.orellana@gmail.com>
2019-02-07 15:59:24 +0100

    Festa

--- Festa.java
+++ Festa.java
@@ -1,6 +1,15 @@
 public class Festa {
     
+    int sexe, drogues, rockandroll;
+    
     Festa(int sexe, int drogues, int rockandroll){
+        this.sexe = sexe;
+        this.drogues = drogues;
+        this.rockandroll = rockandroll;       
     }
+    
+    int porqueriaVaria(){
+    }
+    
     
 }
